a852044e06849be54a2481b89cb5bc522ec92ac4
Nicodemuss <vitornicodemus@gmail.com>
2022-02-15 16:46:47 -0300

    verificação de medico e paciente no recibo

--- back/src/main/java/org/alterdata/shopback/app/serviceimpl/ReciboServiceImpl.java
+++ back/src/main/java/org/alterdata/shopback/app/serviceimpl/ReciboServiceImpl.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.alterdata.shopback.app.dto.ExibicaoCamposDTO;
-import org.alterdata.shopback.app.dto.ReciboDTO;
-import org.alterdata.shopback.app.dto.ReciboTemplateDTO;
+import org.alterdata.shopback.app.dto.*;
 import org.alterdata.shopback.app.exceptions.IdException;
+import org.alterdata.shopback.app.model.Paciente;
 import org.alterdata.shopback.app.model.Recibo;
 import org.alterdata.shopback.app.relatorios.EmissorRecibo;
+import org.alterdata.shopback.app.repository.MedicoRepository;
+import org.alterdata.shopback.app.repository.PacienteRepository;
 import org.alterdata.shopback.app.repository.ReciboRepository;
 import org.alterdata.shopback.app.service.ReciboService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,12 @@ public class ReciboServiceImpl implements ReciboService {
 
 	@Autowired
 	EmissorRecibo emissorRecibo;
+
+	@Autowired
+	MedicoRepository medicoRepository;
+
+	@Autowired
+	PacienteRepository pacienteRepository;
 	
 	// Autowired
 //	private MailConfig mailConfig;
@@ -50,6 +57,7 @@ public class ReciboServiceImpl implements ReciboService {
 
 	@Override
 	public ReciboDTO inserirRecibo(Recibo recibo) {
+
 		reciboRepository.save(recibo);
 		return new ReciboDTO(recibo);
 	}
@@ -62,6 +70,11 @@ public class ReciboServiceImpl implements ReciboService {
 	@Override
 	public InputStream exportarPdf(ReciboTemplateDTO reciboTemplateDTO){
 		Recibo recibo = new Recibo(reciboTemplateDTO);
+		MedicoDTO med = medicoRepository.findByNomeMedico(recibo.getNomeMedico());
+		Paciente pac = pacienteRepository.findByNomePaciente(recibo.getNomePaciente());
+		if(pac == null || med == null){
+			throw new NullPointerException("um dos campos esta vazio");
+		}
 		reciboRepository.save(recibo);
 		return emissorRecibo.emitirRelatorio(reciboTemplateDTO);
 	}
